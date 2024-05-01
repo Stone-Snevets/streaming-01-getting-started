45adad75f47ab833c63d103c846b978e095a15e1
Solomon Stevens <funnyquestions4jbq@gmail.com>
2024-05-01 09:48:14 -0400

    Convert temperature(K to F)

--- process_batch_C.py
+++ process_batch_C.py
@@ -4,6 +4,10 @@ Batch Process C: Third transformation
 Read from a file, transform, write to a new file.
 In this case, covert degree K to degree F.
 
+Kelvin to Fahrenheit calculation
+Fahrenheit = ((Kelvin - 273.15) * 1.8) + 32
+
+
 Note: 
 This is a batch process, but the file objects we use are 
 often called 'file-like objects' or 'streams'.
@@ -34,10 +38,59 @@ OUTPUT_FILE_NAME = "batchfile_3_farenheit.csv"
 
 
 def convert_k_to_f(temp_k):
-    return
+    """ Convert Kelvin to Fahrenheit
+    Use built-in round() to round the answer to 2 decimal places
+    Use built-in float() to convert the string input to a float
+    """
+    logging.debug(f'Calling convert_k_to_f with {temp_k}.')
+
+    # Convert the temperature to degrees Fahrenheit
+    temp_f = round(((float(temp_k) - 273.15) * 1.8) + 32, 2)
+
+    logging.debug(f'Converted {temp_k}K to {temp_f}F.')
+
+    # Return the Fahnreheit temperature
+    return temp_f
 
 
 def process_rows(input_file_name, output_file_name):
+    """ Read in a file, Convert the temperature, Write to a new file"""
+    logging.info(f'Calling process_rows(): {input_file_name} to {output_file_name}.')
+
+    # Open the input file to Read
+    with open(input_file_name, 'r') as input_file:
+        logging.info(f'Opened for reading: {input_file_name}')
+
+        # Create a CSV reader object
+        reader = csv.reader(input_file, delimiter=',')
+
+        # Grab the original header row to skip
+        header = next(reader)
+        logging.info(f'Skipped header row: {header}')
+
+        # Create an output file to write our converted temperatures to
+        #-> Set the newline character to ' ' to avoid unnecessary newlines
+        with open(output_file_name, 'w', newline='') as output_file:
+            logging.info(f'Create output file: {output_file_name}.')
+
+            # Create a csv writer object
+            writer = csv.writer(output_file, delimiter=',')
+
+            # Write a new header row
+            writer.writerow(['Year', 'Month', 'Day', 'Time', 'TempF'])
+
+            # For each row of elements in input_file's list
+            for row in reader:
+                # Assign each measurement to its respective element in the row
+                Year, Month, Day, Time, TempK = row
+
+                # Call convert_k_to_f() to convert the Kelvin temperature to Fahrenheit
+                TempF = convert_k_to_f(TempK)
+
+                # Write the measurements in the output file
+                writer.writerow([Year, Month, Day, Time, TempF])
+
+    # Exit the function
     return
 
 
